a7cb0a4cf6dde842b891adf5c252170770ccdc55
Kevin <90816621+1forb1dden1@users.noreply.github.com>
2022-05-02 21:04:37 -0400

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "LargeInt.h"
+
+void IterateFront(LargeInt<int>& L1, LinkedList<int>::Iterator i)
+{
+  std::cout << "\n";  
+  LinkedList<int> l = L1.getList();
+  (L1.getNegative() == true) ? std::cout << "-": std::cout << "";
+  for (auto i = l.firstnode(); i != l.end(); ++i)
+  {
+    std::cout << *i << "";
+  }
+  std::cout << "\n";
+}
+void IterateBack(LargeInt<int>& L1, LinkedList<int>::Iterator i)
+{
+  std::cout << "\n";
+  LinkedList<int> l = L1.getList();
+  (L1.getNegative() == true) ? std::cout << "-" : std::cout << "+";
+  for (auto i = l.lastnode(); i != l.end(); --i)
+  {
+    std::cout << *i;
+  }
+  std::cout << "\n";
+}
+
+int main()
+{ 
+  std::string input1; std::string input2;
+  LargeInt<int> L1; LargeInt<int> L2;
+  LinkedList<int> List;
+  LinkedList<int>::Iterator iterator;
+  std::cout << "Enter the first number: "; std::cin >> input1;
+  std::cout << "Enter the second number: "; std::cin >> input2;
+  L1.setList(input1);
+  L2.setList(input2);
+  L1 - L2;
+  IterateFront(L1, iterator);
+}
